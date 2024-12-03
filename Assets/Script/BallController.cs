95e3ddfce2cba691102185b9c00d1dd8019589c0
Aykut <kutay-baglan@hotmail.com>
2024-12-03 10:57:00 +0300

    Project Upload

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody myRigidbody;
+    [SerializeField] private Vector2 velocityLimitMinMax;
+    [SerializeField] private GameObject losePanel;
+    [SerializeField] private GameObject winPanel;
+    public Text scoreText;
+    private int score;
+  
+    
+
+    private void Start()
+    {
+        score = 0;
+        scoreText.text = "Score : " + score;
+        // Score : 0
+       
+        
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        var vertical = myRigidbody.velocity;
+        vertical.y = Math.Clamp(vertical.y, velocityLimitMinMax.x, velocityLimitMinMax.y);
+        myRigidbody.velocity = vertical;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "die")
+        {
+            Time.timeScale = 0;
+            
+            losePanel.SetActive(true);
+        }
+        if (collision.gameObject.tag == "win")
+        {
+            Time.timeScale = 0;
+            winPanel.SetActive(true);
+            
+        }
+
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("points"))
+        {
+            score++;
+            scoreText.text = "Score : " + score;
+            
+         
+        }
+    }
+
+
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        losePanel.SetActive(false);
+        winPanel.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+
+}
+ 
+
---
+++ Assets/Script/CircleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CircleController : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+    public List<GameObject> pieces;
+
+    public void PrepareSimple(int nonactiveAmount)
+    {
+        int index = Random.Range(0, pieces.Count);
+
+        for (int i = 0; i < nonactiveAmount; i++)
+        {
+            var item = pieces[index];
+            item.SetActive(false);
+            index++;
+            if (index >= pieces.Count)
+                index = 0;
+        }
+       
+    }
+
+    public void UpdateScore()
+    {
+        score += 10;
+        scoreText.text = "Score: " + score;
+    }
+
+    
+}
