506d4633277091e49e7cf2e1e340d2e4f2b77f84
Aykut <kutay-baglan@hotmail.com>
2025-01-09 12:50:10 +0300

    made the game close when the escape key is pressed

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,6 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
     public static void GameResume()
     {
         Time.timeScale = 1f;
@@ -15,5 +22,9 @@ public class GameManager : MonoBehaviour
     public static void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    private void QuitGame()
+    {
+        Application.Quit();
     }
 }
